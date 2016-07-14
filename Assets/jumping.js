0a7afd823f9365f0a76f18a7651f65f62f034905
Mackenzie Boudreau <mackenzieboudreau@Mackenzies-iMac.local>
2016-07-13 23:41:58 -0300

    First commit

---
+++ Assets/jumping.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var jumpingHeight = 3;
+private var rb:Rigidbody2D;
+
+
+function Start () 
+{
+	rb = this.GetComponent.<Rigidbody2D>();
+}
+
+function FixedUpdate () 
+{
+	if (Input.GetKeyDown(KeyCode.Space)) {
+		var force : Vector2 = Vector2(0,120); 
+		rb.AddForce(force);
+	}
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		this.transform.position.x -= 1 * Time.deltaTime;
+	}
+	if (Input.GetKey(KeyCode.RightArrow)) {
+		this.transform.position.x += 1 * Time.deltaTime;
+	}
+ } 
+
